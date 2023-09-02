2912c5dde1f3b8495d1c68c7057686ab93bdbc37
RealKrupix <piotr.krupa@tutanota.com>
2023-09-02 13:03:31 +0200

    Final changes

--- mail.php
+++ mail.php
@@ -1,18 +1,27 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+    if ($name && $email && $message) {
+        $mailheader = "From: $name <$email>\r\n";
+        $recipient = "konopskajulia6@gmail.com";
+        $subject = "Contact Form Submission from $name";
 
-$mailheader = "From:".$name."<".$email.">\r\n";
-
-$recipient = "konopskajulia6@gmail.com";
-
-mail($recipient, $message, $mailheader)
-or die("Error!");
-
-echo"message send";
-
-
-
+        if (mail($recipient, $subject, $message, $mailheader)) {
+            // Email sent successfully
+            // You can add any additional actions here if needed
+            echo "Message sent successfully!";
+        } else {
+            // Error occurred while sending email
+            // Log the error or provide an error message to the user
+            echo "Error: Unable to send the email. Please try again later.";
+        }
+    } else {
+        // Invalid input data
+        // You can provide an error message or redirect the user back to the form
+        echo "Error: Please fill in all the required fields.";
+    }
+}
 ?>
